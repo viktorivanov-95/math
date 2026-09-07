ab35cb6b535956e519b937956747a6fe32e5064b
Viktor Ivanov <viktorivanov95@mail.ru>
2026-09-07 23:14:55 +0300

    Update polyadic_system_checking_number.py

--- polyadic_system_checking_number.py
+++ polyadic_system_checking_number.py
@@ -2,184 +2,187 @@ import numpy
 import time
 import tracemalloc
 
-def optimized_opss_conversion(osn_inform, osn_kontrol, chisl):
-    osn = numpy.concatenate((osn_inform, osn_kontrol))
-    osn_kolvo = len(osn)
+def opss_conversion(bases, residues):
+    """
+    Перевод числа из модулярной системы в десятичную с помощью ОПСС
+    """
+    bases_count = len(bases)
+    full_diapazon = numpy.prod(bases.astype(object))
     
-    #Поиск констант c_ij:
-    c = numpy.zeros((osn_kolvo-1, osn_kolvo), dtype=object)
+    print("\n1. Вычисление полного диапазона:")
+    print(f"Полный диапазон P = {full_diapazon}")
     
-    #Вычисляем только нужные константы (нижняя треугольная матрица)
-    for j in range(osn_kolvo-1):
+    # 2. Поиск констант c_ij (обратных элементов)
+    print("\n2. Поиск констант c_ij (обратных элементов):")
+    c = numpy.zeros((bases_count-1, bases_count), dtype=object)
+    
+    for j in range(bases_count-1):
         for i in range(j+1):
             try:
-                c[j][i] = pow(int(osn[i]), -1, int(osn[j+1]))
-                print(f'c[{j}][{i}] = {c[j][i]} (поскольку {c[j][i]} * {osn[i]} ≡ 1 mod {osn[j+1]})')
+                c[j][i] = pow(int(bases[i]), -1, int(bases[j+1]))
+                print(f'c[{j}][{i}] = {c[j][i]} (поскольку {c[j][i]} * {bases[i]} ≡ 1 mod {bases[j+1]})')
             except ValueError:
-                print(f'Невозможно найти c[{j}][{i}] - обратный элемент для {osn[i]} mod {osn[j+1]}')
-                return None
+                print(f'Невозможно найти c[{j}][{i}] - обратный элемент для {bases[i]} mod {bases[j+1]}')
+                return None, None
     
-    #Матрица констант c_ij:
-    print(c[:,:osn_kolvo-1])
+    print("\nМатрица констант c_ij:")
+    print(c[:,:bases_count-1])
     
-    #Расчет числа в ОПСС:
-    opss = numpy.zeros(osn_kolvo, dtype=object)
-    opss[0] = chisl[0] % osn[0]
-    print(f'opss[0] = {chisl[0]} mod {osn[0]} = {opss[0]}')
+    # 3. Расчет числа в ОПСС
+    print("\n3. Расчет числа в ОПСС:")
+    opss = numpy.zeros(bases_count, dtype=object)
     
-    if osn_kolvo > 1:
-        temp = (chisl[1] - opss[0]) * c[0][0]
-        opss[1] = temp % osn[1]
-        print(f'opss[1] = ({chisl[1]} - {opss[0]}) * {c[0][0]} = {temp} mod {osn[1]} = {opss[1]}')
+    opss[0] = residues[0] % bases[0]
+    print(f'opss[0] = {residues[0]} mod {bases[0]} = {opss[0]}')
     
-    for i in range(2, osn_kolvo):
-        #Первый шаг вычисления
-        opss[i] = (chisl[i] - opss[0]) * c[i-1][0]
-        print(f'\nНачальное opss[{i}] = ({chisl[i]} - {opss[0]}) * {c[i-1][0]} = {opss[i]}')
+    if bases_count > 1:
+        temp = (residues[1] - opss[0]) * c[0][0]
+        opss[1] = temp % bases[1]
+        print(f'opss[1] = ({residues[1]} - {opss[0]}) * {c[0][0]} = {temp} mod {bases[1]} = {opss[1]}')
+    
+    for i in range(2, bases_count):
+        # Первый шаг вычисления
+        opss[i] = (residues[i] - opss[0]) * c[i-1][0]
+        print(f'\nНачальное opss[{i}] = ({residues[i]} - {opss[0]}) * {c[i-1][0]} = {opss[i]}')
         
-        #Коррекция с использованием предыдущих значений
+        # Коррекция с использованием предыдущих значений
         for j in range(1, i):
             opss[i] = (opss[i] - opss[j]) * c[i-1][j]
             print(f'Шаг {j}: opss[{i}] = (пред.результат - {opss[j]}) * {c[i-1][j]} = {opss[i]}')
         
-        opss[i] %= osn[i]
-        print(f'Финальное opss[{i}] = {opss[i]} mod {osn[i]}')
+        opss[i] %= bases[i]
+        print(f'Финальное opss[{i}] = {opss[i]} mod {bases[i]}')
     
-    print('\nЧисло в ОПСС равно:', opss)
-    return opss
+    print(f'\nЧисло в ОПСС: {opss}')
+    
+    # 4. Перевод из ОПСС в десятичную систему
+    print("\n4. Перевод из ОПСС в десятичную систему:")
+    decimal_number = 0
+    for i in range(bases_count):
+        product = numpy.prod(bases[:i].astype(object)) if i > 0 else 1
+        term = opss[i] * product
+        print(f'{opss[i]} * {product} = {term}')
+        decimal_number += term
+    
+    print(f'\nИтоговое десятичное число: {decimal_number}')
+    
+    return opss, decimal_number
 
-#Проверка методом ортогональных базисов
-def check_with_orthogonal_bases(osn_inform, osn_kontrol, chisl):
-    osn = numpy.concatenate((osn_inform, osn_kontrol))
-    osn_kolvo = len(osn)
-    full_diapazon = numpy.prod(osn.astype(object))
+def check_with_orthogonal_bases(bases, residues):
+    """
+    Проверка методом ортогональных базисов (алгоритм из скрипта orthogonal_bases)
+    """
+    print("\n1. Исходные данные для проверки:")
+    print(f"Основания системы: {bases}")
+    print(f"Остатки числа: {residues}")
     
-    print('\n1. Поиск величин P/pi = Pi для каждого основания:')
-    P = numpy.zeros(osn_kolvo, dtype=object)
-    for i in range(osn_kolvo):
-        P[i] = full_diapazon // osn[i]
-        print(f'P[{i}] = {full_diapazon} / {osn[i]} = {P[i]}')
-        
-    print('\n2. Поиск величин βi = Pi mod pi:')
-    beta = numpy.zeros(osn_kolvo, dtype=object)
-    for i in range(osn_kolvo):
-        beta[i] = P[i] % osn[i]
-        print(f'β[{i}] = {P[i]} mod {osn[i]} = {beta[i]}')
+    # Вычисление полного диапазона
+    print("\n2. Вычисление полного диапазона:")
+    full_diapazon = numpy.prod(bases.astype(object))
+    print(f"Полный диапазон P (произведение всех оснований) = {full_diapazon}")
     
-    print('\n3. Нахождение весов mi базисов (mi * βi ≡ 1 mod pi):')
-    m = numpy.zeros(osn_kolvo, dtype=object)
-    for i in range(osn_kolvo):
+    # Вычисление ортогональных базисов
+    print("\n3. Вычисление ортогональных базисов:")
+    bases_count = len(bases)
+    
+    # 3.1 Поиск величины P/pi = Pi для каждого основания
+    print("\n3.1 Поиск величины P/pi = Pi для каждого основания:")
+    P = numpy.zeros(bases_count, dtype=object)
+    for i in range(bases_count):
+        P[i] = full_diapazon // bases[i]
+        print(f"P[{i}] = {full_diapazon} // {bases[i]} = {P[i]}")
+    print(f"Величина P = {P}")
+    
+    # 3.2 Поиск величины βi = Pi(mod pi)
+    print("\n3.2 Поиск величины βi = Pi(mod pi):")
+    beta = numpy.zeros(bases_count, dtype=object)
+    for i in range(bases_count):
+        beta[i] = P[i] % bases[i]
+        print(f"β[{i}] = {P[i]} mod {bases[i]} = {beta[i]}")
+    print(f"Величина β = {beta}")
+    
+    # 3.3 Нахождение веса mi базисов (обратный элемент βi по модулю pi)
+    print("\n3.3 Нахождение весов mi базисов (обратный элемент βi по модулю pi):")
+    m = numpy.zeros(bases_count, dtype=object)
+    for i in range(bases_count):
         try:
-            m[i] = pow(int(beta[i]), -1, int(osn[i]))
-            print(f'm[{i}] = {beta[i]}^(-1) mod {osn[i]} = {m[i]} (поскольку {m[i]} * {beta[i]} ≡ 1 mod {osn[i]})')
+            m[i] = pow(int(beta[i]), -1, int(bases[i]))
+            print(f"m[{i}] = inv({beta[i]}) mod {bases[i]} = {m[i]}")
         except ValueError:
-            print(f'Yевозможно найти обратный элемент для β[{i}] = {beta[i]} по модулю {osn[i]}')
+            print(f"Ошибка: невозможно найти обратный элемент для beta[{i}] = {beta[i]} по модулю {bases[i]}")
             return None
+    print(f"Веса базисов m = {m}")
     
-    print('\n4. Вычисление ортогональных базисов системы Bi = mi * Pi:')
-    B = numpy.zeros(osn_kolvo, dtype=object)
-    for i in range(osn_kolvo):
+    # 3.4 Вычисление ортогональных базисов системы Bi = mi * Pi
+    print("\n3.4 Вычисление ортогональных базисов системы Bi = mi * Pi:")
+    B = numpy.zeros(bases_count, dtype=object)
+    for i in range(bases_count):
         B[i] = m[i] * P[i]
-        print(f'B[{i}] = {m[i]} * {P[i]} = {B[i]}')
+        print(f"B[{i}] = {m[i]} * {P[i]} = {B[i]}")
+    print(f"Ортогональные базисы системы B = {B}")
     
-    print('\n5. Нахождение числа A в позиционной системе счисления:')
+    # 3.5 Нахождение числа A в десятичной системе
+    print("\n3.5 Нахождение числа A в десятичной системе:")
     A = 0
-    for i in range(osn_kolvo):
-        prev_A = A
-        A += chisl[i] * B[i]
-        print(f'A[{i}] = {prev_A} + {chisl[i]} * {B[i]} = {A}')
+    for i in range(bases_count):
+        old_A = A
+        A += residues[i] * B[i]
+        print(f"A[{i}] = {old_A} + {residues[i]} * {B[i]} = {A}")
     
     A = A % full_diapazon
-    print(f'\nИтоговое A = {A} mod {full_diapazon} = {A}')
+    print(f"\nA = {A} (после взятия по модулю {full_diapazon})")
+    
     return A
 
-#Начало программы
+# Начало программы
 start_time = time.time()
 tracemalloc.start()
 
-print('Основания нужно вводить в порядке возрастания: сначала информационные, затем контрольные. Они должны быть взаимно простыми')
-osn_inform_kolvo = int(input('Введите количество информационных оснований: '))
-osn_kontrol_kolvo = int(input('Введите количество контрольных оснований: '))
-osn_inform = numpy.zeros((osn_inform_kolvo), dtype=object)
-osn_kontrol = numpy.zeros((osn_kontrol_kolvo), dtype=object)
+print("Введите основания системы (все основания должны быть попарно взаимно простыми):")
+bases_count = int(input('Введите количество оснований: '))
+bases = numpy.zeros(bases_count, dtype=object)
 
-for i in range(osn_inform_kolvo):
-    osn_inform[i] = int(input(f'Введите {i+1} информационное основание: '))
-for i in range(osn_kontrol_kolvo):
-    osn_kontrol[i] = int(input(f'Введите {i+1} контрольное основание: '))
+for i in range(bases_count):
+    bases[i] = int(input(f'Введите {i+1} основание: '))
 
 print('\nВведите остатки числа по каждому основанию:')
-chisl = numpy.zeros(len(osn_inform) + len(osn_kontrol), dtype=object)
-for i in range(len(osn_inform)):
-    chisl[i] = int(input(f'Введите остаток по информационному основанию {osn_inform[i]}: '))
-for i in range(len(osn_kontrol)):
-    chisl[len(osn_inform)+i] = int(input(f'Введите остаток по контрольному основанию {osn_kontrol[i]}: '))
+residues = numpy.zeros(bases_count, dtype=object)
+for i in range(bases_count):
+    residues[i] = int(input(f'Введите остаток по основанию {bases[i]}: '))
 
-osn = numpy.concatenate((osn_inform, osn_kontrol))
-work_diapazon = numpy.prod(osn_inform.astype(object))
-full_diapazon = numpy.prod(osn.astype(object))
+full_diapazon = numpy.prod(bases.astype(object))
 
 print('\nВведенные данные:')
-print('Информационные основания:', osn_inform)
-print('Контрольные основания:', osn_kontrol)
-print('Все основания:', osn)
-print('Остатки числа:', chisl)
-print(f'Рабочий диапазон R = {work_diapazon}')
+print(f'Основания: {bases}')
+print(f'Остатки числа: {residues}')
 print(f'Полный диапазон P = {full_diapazon}')
 
-#Ппроверка методом ОПСС
-print('Перевод числа из СОК в ОПСС')
-opss = optimized_opss_conversion(osn_inform, osn_kontrol, chisl)
+# Перевод числа из СОК в десятичную систему через ОПСС
+print('Перевод числа из модулярной системы счисления в позиционную (десятичную) методом перевода в ОПСС')
+opss, decimal_number = opss_conversion(bases, residues)
 
 if opss is not None:
-    #Проверка на ошибки по контрольным основаниям
-    print('\nПроверка числа на наличие ошибок:')
-    error_found = False
-    for i in range(osn_inform_kolvo, len(osn)):
-        if opss[i] != 0:
-            print(f'Обнаружена ошибка: opss[{i}] = {opss[i]} ≠ 0 (по контрольному основанию {osn[i]})')
-            error_found = True
-
-    if not error_found:
-        print('Ошибок не обнаружено: все контрольные остатки в ОПСС равны 0')
-
-    #Перевод ОПСС в десятичное число
-    print('\nПеревод числа из ОПСС в десятичную систему:')
-    opss_check = 0
-    for i in range(len(osn)):
-        product = numpy.prod(osn[:i].astype(object)) if i > 0 else 1
-        term = opss[i] * product
-        print(f'{opss[i]} * {product} = {term}')
-        opss_check += term
-    print(f'Итоговое число: {opss_check}')
-
-#Проверка методом ортогональных базисов
-print('Проверка методом ортогональных базисов')
-A_orth = check_with_orthogonal_bases(osn_inform, osn_kontrol, chisl)
-
-#Сравнение результатов
-if opss is not None and A_orth is not None:
-    print('Сравнение результатов')
-    print(f'Число из ОПСС: {opss_check}')
-    print(f'Число из ортогональных базисов: {A_orth}')
-
-    if opss_check == A_orth:
-        print('Результаты совпадают')
-    else:
-        print('Результаты не совпадают!')
+    # Проверка методом ортогональных базисов
+    print('Проверка методом ортогональных базисов')
+    A_orth = check_with_orthogonal_bases(bases, residues)
     
-    #Проверка на ошибки по рабочему диапазону
-    if A_orth > work_diapazon:
-        print(f'Обнаружена ошибка: число {A_orth} находится вне рабочего диапазона (0-{work_diapazon-1})')
-    else:
-        print(f'Число {A_orth} находится в рабочем диапазоне (0-{work_diapazon-1})')
+    # Сравнение результатов
+    if A_orth is not None:
+        print('Сравнение результатов')
+        print(f'Число из ОПСС: {decimal_number}')
+        print(f'Число из ортогональных базисов: {A_orth}')
+        
+        if decimal_number == A_orth:
+            print('Результаты совпадают!')
+        else:
+            print('Результаты не совпадают!')
 
-#Замер памяти и времени
+# Замер памяти и времени
 current, peak = tracemalloc.get_traced_memory()
 tracemalloc.stop()
 execution_time = time.time() - start_time
 
-print('Ресурсы программы:')
 print(f'Использовано памяти: {current/1024:.2f} KB')
 print(f'Пиковое использование памяти: {peak/1024:.2f} KB')
 print(f'Время выполнения: {execution_time * 1e6:.2f} микросекунд')
+print(f'Время выполнения: {execution_time:.6f} секунд')
